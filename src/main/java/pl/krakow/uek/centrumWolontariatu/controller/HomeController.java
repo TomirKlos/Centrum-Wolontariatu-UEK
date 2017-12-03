5c56b31b77cf663d581584a2c47bc8779198014d
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-12-03 20:28:54 +0100

    added spring-boot template

---
+++ src/main/java/pl/krakow/uek/centrumWolontariatu/controller/HomeController.java
@@ -0,0 +1,20 @@
+package pl.krakow.uek.centrumWolontariatu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by MSI DRAGON on 2017-12-03.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+}
