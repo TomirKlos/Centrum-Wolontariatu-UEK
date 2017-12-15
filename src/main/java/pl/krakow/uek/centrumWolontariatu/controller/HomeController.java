72946d1d08f61445acf9313afc049900de62b51a
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-12-15 17:16:37 +0100

    changed view resolver to thymyleaf from jsp in homeController

--- src/main/java/pl/krakow/uek/centrumWolontariatu/controller/HomeController.java
+++ src/main/java/pl/krakow/uek/centrumWolontariatu/controller/HomeController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by MSI DRAGON on 2017-12-03.
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @RequestMapping("/")
     public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "home";
+        return "thymeleaf/home";
     }
 }
