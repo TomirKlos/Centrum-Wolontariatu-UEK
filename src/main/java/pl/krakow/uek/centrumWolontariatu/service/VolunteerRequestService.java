d4e500895fdba75a2227f0bc00c85236540395fe
Adrian Scehura <adrian.scehura@gmail.com>
2018-03-12 19:17:48 +0100

    reset master

--- src/main/java/pl/krakow/uek/centrumWolontariatu/service/VolunteerRequestService.java
+++
@@ -1,6 +0,0 @@
-package pl.krakow.uek.centrumWolontariatu.service;
-
-import pl.krakow.uek.centrumWolontariatu.domain.repository.VolunteerRequestRepository;
-
-public interface VolunteerRequestService extends VolunteerRequestRepository {
-}
