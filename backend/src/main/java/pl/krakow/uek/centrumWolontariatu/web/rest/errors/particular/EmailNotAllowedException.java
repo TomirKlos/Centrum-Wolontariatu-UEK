21eaac8cbcfbe52712a498efc3e2c07f0fe4fbfe
Tomir Kłós <31741223+TomirKlos@users.noreply.github.com>
2018-03-18 17:29:27 +0100

    add errorKey to emailNotAllowedException

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/errors/particular/EmailNotAllowedException.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/errors/particular/EmailNotAllowedException.java
@@ -2,11 +2,13 @@ package pl.krakow.uek.centrumWolontariatu.web.rest.errors.particular;
 
 import org.zalando.problem.AbstractThrowableProblem;
 import org.zalando.problem.Status;
+import pl.krakow.uek.centrumWolontariatu.web.rest.errors.general.BadRequestAlertException;
 
-public class EmailNotAllowedException extends AbstractThrowableProblem {
+public class EmailNotAllowedException extends BadRequestAlertException {
 
         public EmailNotAllowedException() {
-            super(null, "Email address not allowed from this domain", Status.BAD_REQUEST);
+            super(null, "Email address not allowed from this domain", "userManagement", "emailbaddomain");
+
         }
 }
 
