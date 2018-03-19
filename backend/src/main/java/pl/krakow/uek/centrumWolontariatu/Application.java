def85e13bf11b3e0d4c796da8daca93a02214ad0
Tomir Kłós <31741223+TomirKlos@users.noreply.github.com>
2018-03-19 11:01:35 +0100

    bugfix exclude MultipartAutoConfiguration

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/Application.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/Application.java
@@ -2,14 +2,16 @@ package pl.krakow.uek.centrumWolontariatu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import pl.krakow.uek.centrumWolontariatu.configuration.AppProperties;
 
+
 @Configuration
-@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class, MultipartAutoConfiguration.class})
 @ComponentScan
 @EnableConfigurationProperties(AppProperties.class)
 public class Application {
