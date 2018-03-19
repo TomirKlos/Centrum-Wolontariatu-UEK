f086d9e1c301f3db65882ad669c4f8bedf7a0c4c
Tomir Kłós <31741223+TomirKlos@users.noreply.github.com>
2018-03-19 13:03:58 +0100

    update EmailNotFoundException with errorKey

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/errors/particular/EmailNotFoundException.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/errors/particular/EmailNotFoundException.java
@@ -2,10 +2,11 @@ package pl.krakow.uek.centrumWolontariatu.web.rest.errors.particular;
 
 import org.zalando.problem.AbstractThrowableProblem;
 import org.zalando.problem.Status;
+import pl.krakow.uek.centrumWolontariatu.web.rest.errors.general.BadRequestAlertException;
 
-public class EmailNotFoundException extends AbstractThrowableProblem {
+public class EmailNotFoundException extends BadRequestAlertException {
 
     public EmailNotFoundException() {
-        super(null, "Email address not registered", Status.BAD_REQUEST);
+        super(null, "Email address not registered", "userManagement", "emailnotfound");
     }
 }
