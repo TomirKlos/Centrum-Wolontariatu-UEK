e98dfa2e4a38274ac75c2093dd1046d0c604531f
Tomir Kłós <31741223+TomirKlos@users.noreply.github.com>
2018-03-25 22:38:23 +0200

    add user activation api for admin

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/UserResource.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/web/rest/UserResource.java
@@ -1,24 +1,30 @@
 package pl.krakow.uek.centrumWolontariatu.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.krakow.uek.centrumWolontariatu.domain.User;
+import pl.krakow.uek.centrumWolontariatu.repository.UserRepository;
+import pl.krakow.uek.centrumWolontariatu.service.MailDomainToAuthoritiesService;
 import pl.krakow.uek.centrumWolontariatu.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserResource {
 
+    private final Logger log = LoggerFactory.getLogger(UserService.class);
     final private UserService userService;
+    private final UserRepository userRepository;
+    private final MailDomainToAuthoritiesService mailDomainToAuthoritiesService;
 
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, UserRepository userRepository, MailDomainToAuthoritiesService mailDomainToAuthoritiesService) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.mailDomainToAuthoritiesService = mailDomainToAuthoritiesService;
     }
 
     @GetMapping
@@ -28,6 +34,12 @@ public class UserResource {
 
         Page<User> page = userService.findAll(pageable, activated);
         return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/activate")
+    @ResponseStatus(HttpStatus.OK)
+    public void activateUser(@RequestParam long id){
+        userService.activateUser(id);
     }
 
 }
