6b5c9bc05f13e8cb632097ffff5696caa660696d
Tomir Kłós <31741223+TomirKlos@users.noreply.github.com>
2018-03-26 22:02:33 +0200

    update static resource configuration

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/StaticResourceConfiguration.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/StaticResourceConfiguration.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import static pl.krakow.uek.centrumWolontariatu.configuration.constant.UserConstant.UPLOADED_FOLDER;
+import static pl.krakow.uek.centrumWolontariatu.configuration.constant.UserConstant.UPLOADED_FOLDER_AD;
+import static pl.krakow.uek.centrumWolontariatu.configuration.constant.UserConstant.UPLOADED_FOLDER_REQUESTS;
 
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-        "file:" + UPLOADED_FOLDER};
+        "file:" + UPLOADED_FOLDER_REQUESTS, "file:" + UPLOADED_FOLDER_AD};
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/constant/UserConstant.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/constant/UserConstant.java
@@ -7,5 +7,6 @@ public class UserConstant {
     public static final int EMAIL_MAX_SIZE = 50;
     public static final int PASSWORD_MIN_LENGTH = 8;
     public static final int PASSWORD_MAX_LENGTH = 100;
-    public static final String UPLOADED_FOLDER = "E://images//";
+    public static final String UPLOADED_FOLDER_REQUESTS = "E://images//requests";
+    public static final String UPLOADED_FOLDER_AD = "E://images//ad";
 }
