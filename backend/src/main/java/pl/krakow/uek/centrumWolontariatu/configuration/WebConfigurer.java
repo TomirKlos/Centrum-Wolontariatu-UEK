a1aed3cd467b8e30d79593cc58ae56521f046f0d
Adrian Scehura <adrian.scehura@gmail.com>
2018-04-02 11:38:34 +0200

    delete old corsFilter

--- backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/WebConfigurer.java
+++ backend/src/main/java/pl/krakow/uek/centrumWolontariatu/configuration/WebConfigurer.java
@@ -25,22 +25,7 @@ public class WebConfigurer {
     ProblemModule problemModule() {
         return new ProblemModule();
     }
-
-//    @Bean
-//    @SuppressWarnings("unchecked")
-//    public FilterRegistrationBean simpleCorsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-//        config.setAllowedMethods(Collections.singletonList("*"));
-//        config.setAllowedHeaders(Collections.singletonList("*"));
-//        source.registerCorsConfiguration("/**", config);
-//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        return bean;
-//    }
-
+    
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
