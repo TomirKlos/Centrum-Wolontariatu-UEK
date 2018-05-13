75cc5437e5f93e634ea803e7b0a1ecefa622a86e
adian <adrian.scehura@gmail.com>
2018-05-13 16:17:21 +0200

    commit before angular 6

---
+++ frontend/src/app/requests/shared/request-resolver.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { VolunteerRequestVM } from '../../../shared/interfaces';
+import { ServerResolverService } from '../../../shared/server-resolver.service';
+import { RequestService } from '../../shared/request.service';
+
+@Injectable()
+export class ViewRequestResolverService extends ServerResolverService<VolunteerRequestVM> {
+
+  constructor(private _requestService: RequestService, private _router: Router) {
+    super(_requestService, _router);
+  }
+}
