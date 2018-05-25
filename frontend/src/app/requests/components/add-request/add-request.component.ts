809fde8f4e00ba9314449541144a5de895913161
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2018-05-25 15:32:04 +0200

    add image upload to 'add-request' form

--- frontend/src/app/requests/components/add-request/add-request.component.ts
+++ frontend/src/app/requests/components/add-request/add-request.component.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { SnackBarService } from '../../../shared/snack-bar.service';
 
+import { ImageUploadModule } from "angular2-image-upload";
+
+
 @Component({
   selector: 'app-add-request',
   templateUrl: './add-request.component.html',
@@ -12,6 +15,11 @@ import { SnackBarService } from '../../../shared/snack-bar.service';
 export class AddRequestComponent implements OnInit {
   formGroup: FormGroup;
   submitButtonDisabled = false;
+
+
+  pathToStaticContent = "http://localhost:8080/static/";
+  selectedFile: File[];
+  fileHash: any[] = [];
 
   constructor(private _fb: FormBuilder, private _http: HttpClient, private _sb: SnackBarService) {
   }
@@ -22,6 +30,7 @@ export class AddRequestComponent implements OnInit {
       description: [ '', [ Validators.required ] ],
       forStudents: [ false ],
       forTutors: [ false ],
+      images: [ this.fileHash ], 
     });
   }
 
@@ -34,5 +43,34 @@ export class AddRequestComponent implements OnInit {
       () => this._sb.warning()
     );
   }
+
+  onFileSelected(event){
+    this.selectedFile = <File[]> event.target.files;
+  }
+
+  onUpload(){
+    const fd = new FormData();
+   /* this.selectedFile.forEach((file) =>{
+      fd.append('file', file, file.name)
+    })*/ 
+    for(var i = 0; i < this.selectedFile.length; i++){
+      fd.append('file', this.selectedFile[i], this.selectedFile[i].name)
+    }
+    this._http.post(environment.apiEndpoint + '/vrequest/picture', fd)
+    .subscribe(res =>{
+      console.log(res);
+      (res as string[]).forEach(element => {
+        this.fileHash.push(element);
+      });
+      console.log(this.fileHash)
+      
+    })
+  }
+
+  deletePicture(doc){
+    this.fileHash.forEach( (item, index) => {
+      if(item === doc) this.fileHash.splice(index,1);
+    });
+ }
 
 }
