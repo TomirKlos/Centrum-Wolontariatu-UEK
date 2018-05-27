716b5871e5bfba9b376b30aeb05902b9e091879e
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2018-05-27 10:48:39 +0200

    relativeDatePipe quick fix import

--- frontend/src/app/requests/shared/main-pipe.module.ts
+++ frontend/src/app/requests/shared/main-pipe.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
-import { RelativeDatePipe } from './relativeDate.pipe';
+import { RelativeDatePipe } from '../../shared/relativeDate.pipe';
+
 
 
 @NgModule({
