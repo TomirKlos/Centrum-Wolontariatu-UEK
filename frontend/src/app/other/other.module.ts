b25c2cbd544f89826e28f12a0f9103bf378f0bac
adian <adrian.scehura@gmail.com>
2018-08-25 12:09:22 +0200

    add prod properties and some polish

--- frontend/src/app/other/other.module.ts
+++ frontend/src/app/other/other.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { OtherRoutingModule } from './other-routing.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { LayoutModule } from '../shared/layout.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    LayoutModule,
     OtherRoutingModule
   ],
   declarations: [
